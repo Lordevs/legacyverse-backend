3983ca6e18f6cdcced0db4c4e0af215d63269d93
arsalanbashir831 <arsalanbashir831@gmail.com>
2025-10-07 01:02:35 +0000

    Remove "https://api.memoriva.org" from CORS_ALLOWED_ORIGINS in settings.py

--- legacyverse/settings.py
+++ legacyverse/settings.py
@@ -147,7 +147,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://api.memoriva.org",
-    "https://api.memoriva.org",
     "https://memoriva.org",
 ]
 
