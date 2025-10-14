ea9cc296080ae0aaec88f456da093ea9cf2ddbca
arsalanbashir831 <arsalanbashir831@gmail.com>
2025-10-14 13:51:31 +0000

    Update CORS_ALLOWED_ORIGINS in settings.py to correct URL formatting for memoriva.org domains

--- legacyverse/settings.py
+++ legacyverse/settings.py
@@ -147,7 +147,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://api.memoriva.org",
-   "https://memoriva.org/","https://www.memoriva.org/"
+    "https://memoriva.org",
+    "https://www.memoriva.org"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
